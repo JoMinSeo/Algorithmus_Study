f70616b4a190616275b9b9908af5148fb451f241
Jominseo <minseocho0309@gmail.com>
2021-01-04 22:46:48 +0900

    마라톤을 완주하지 못한 선수 문제 풀이

---
+++ src/marathon.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+/* 프로그래머스 : 완주하지 못한 선수 */
+public class marathon {
+
+    public String marathon(String[] participant, String[] completion) {
+        String answer = "";
+        String temp = "";
+
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        int i = 0;
+
+        while (i < participant.length) {
+            if(!completion[i].equals(participant[i])){
+                temp = participant[i];
+                break;
+            }else{
+                i++;
+            }
+        }
+
+        if(!temp.equals("")){
+            answer = temp;
+        }else{
+            answer = participant[participant.length - 1];
+        }
+
+        return answer;
+    }
+
+}
